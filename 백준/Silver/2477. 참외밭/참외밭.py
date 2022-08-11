705e00939ddae48acb29849b3877a780cb1236bb
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2022-08-11 11:23:49 +0900

    [Silver III] Title: 참외밭, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -5,6 +5,7 @@ for _ in range(6):
     length_info_list.append((direction, length))
 
 
+# 큰 사각형을 구하기 
 row_list = []
 column_list = []
 for length_info in length_info_list:
@@ -14,24 +15,26 @@ for length_info in length_info_list:
     if length_info[0] == 3 or length_info[0] == 4:
         column_list.append(length_info[1])
 
+# 큰사각형의 가로, 세로
+large_row_length = max(row_list)
+large_column_length = max(column_list)
+row_column_max_length = [large_row_length, large_column_length]
 
-row_max_length = max(row_list)
-column_max_length = max(column_list)
+#[(4, 50), (2, 160), (3, 30), (1, 60), (3, 20), (1, 100), (4, 50), (2, 160), (3, 30), (1, 60), (3, 20), (1, 100)]
+#   r         c                  x        y
+#[(3, 20), (1, 100), (4, 50), (2, 160), (3, 30), (1, 60), (3, 20), (1, 100), (4, 50), (2, 160), (3, 30), (1, 60)]
+#                       r        c                  x         y
+#[(3, 30), (1, 60), (3, 20), (1, 100), (4, 50), (2, 160), (3, 30), (1, 60), (3, 20), (1, 100), (4, 50), (2, 160)]
+#                                         r        c                  x        y
 
-[(4, 50), (2, 160), (3, 30), (1, 60), (3, 20), (1, 100)]
-#   x        y
-[(3, 20), (1, 100), (4, 50), (2, 160), (3, 30), (1, 60)]
-#                     x         y
-[(3, 30), (1, 60), (3, 20), (1, 100), (4, 50), (2, 160)]
-#                                        x        y
-
+# 작은 사각형의 가로 세로 x, y의 길이는 r을 기준으로 3칸과 4칸 뒤에 있는 값이다.
 double_length_info_list = length_info_list * 2
 
 for i in range(len(double_length_info_list)):
-    if double_length_info_list[i][1] == row_max_length or double_length_info_list[i][1] == column_max_length:
-        if double_length_info_list[i+1][1] == row_max_length or double_length_info_list[i+1][1] == column_max_length:
-            x = double_length_info_list[i+3][1]
-            y = double_length_info_list[i+4][1]
+    if double_length_info_list[i][1] in row_column_max_length:
+        if double_length_info_list[i+1][1] in row_column_max_length:
+            small_row_length = double_length_info_list[i+3][1]
+            small_column_length = double_length_info_list[i+4][1]
 
-            print((row_max_length*column_max_length - x*y)*n)
+            print((large_row_length*large_column_length - small_row_length*small_column_length) * n)
             break
