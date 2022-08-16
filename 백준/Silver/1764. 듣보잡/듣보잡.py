ea9a4fd4da69f853a1ec85b3dfe2ba5d24cfabc1
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2022-08-16 19:26:15 +0900

    [Silver IV] Title: 듣보잡, Time: 240 ms, Memory: 128548 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,19 +1,11 @@
 n, m = map(int, input().split())
 
-unheard_name_list = []
-unseen_name_list = []
-for i in range(n+m):
-    name = input()
-    if i < n:
-        unheard_name_list.append(name)
-    
-    else:
-        unseen_name_list.append(name)
+unheard_name_list = [input() for _ in range(n)]
+unseen_name_list = [input() for _ in range(m)]
 
 answer = list(set(unheard_name_list) & set(unseen_name_list))
 
 answer.sort()
 print(len(answer))
-if answer:
-    for name in answer:
-        print(name)
+for name in answer:
+    print(name)
