969771ba67c3ead41cf3be483e2848f464dfc94d
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2022-08-19 10:08:15 +0900

    [Silver V] Title: 나이순 정렬, Time: 372 ms, Memory: 137504 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-result = [input().split() for _ in range(n)]
-result.sort(key=lambda x: int(x[0]))
+user_info_list = [input().split() for _ in range(n)]
+user_info_list.sort(key=lambda x: int(x[0]))
 
-for i in result:
-    print(i[0], i[1])
+for user in user_info_list:
+    print(user[0], user[1])
