941c276ca0d15764906ce83fc00bc5588252faad
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2024-01-10 16:25:18 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 114036 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -14,28 +14,28 @@ def bfs(maps, start=[0,0]):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1:
+            if 0 <= nx < len(maps[0]) and 0 <= ny < len(maps) and maps[ny][nx] == 1:
                 maps[ny][nx] = 0
                 q.append([ny, nx])
     return maps
 
-t = int(input())
-answer = []
-for _ in range(t):
-    m, n, k= map(int, input().split())
-    # 스텝 1: 양배추 for문으로 maps 업데이트
-    cabages = [list(map(int, input().split())) for _ in range(k)]
-    maps = [[0]*m for _ in range(n)]
-    for x in cabages:
-        maps[x[1]][x[0]] = 1
+def solution(t: int) -> list[int]:
+    answer = []
+    for _ in range(t):
+        m, n, k= map(int, input().split())
+        # 스텝 1: 양배추 for문으로 maps 업데이트
+        cabages = [list(map(int, input().split())) for _ in range(k)]
+        maps = [[0]*m for _ in range(n)]
+        for x in cabages:
+            maps[x[1]][x[0]] = 1
 
-    result = 0
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 1:
-                bfs(maps, [i, j])
-                result += 1
-    answer.append(result)
+        result = 0
+        for i in range(n):
+            for j in range(m):
+                if maps[i][j] == 1:
+                    bfs(maps, [i, j])
+                    result += 1
+        answer.append(str(result))
+    return '\n'.join(answer)
 
-for x in answer:
-    print(x)
+print(solution(int(input())))
