27114858e21fa1fa33716fce4983bac835868e5e
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2024-01-10 11:33:07 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,7 +5,6 @@ n, m= map(int, input().split())
 maps = [input() for _ in range(n)]
 
 def bfs(maps):
-
     q = deque() #패킹해서 넣어줘야 한다.   
     q.append([0,0])
     distance = [[0]*len(maps[0]) for _ in range(len(maps))] # 거리 기록
@@ -13,20 +12,18 @@ def bfs(maps):
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-
     #해당 시작점에서 주변의 1이 없는지 확인
     while q:
-        y, x = q.popleft()
+        x, y = q.popleft()
 
         #시작점 기준으로 상하좌우의 노드를 확인
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if 0<=nx<len(maps[0]) and 0<=ny<len(maps) and maps[ny][nx] == '1': #방문하지 않는 노드이면서 범위 안에 있을 때,
-                if distance[ny][nx] == 0:
-                    q.append([ny, nx])
-                    distance[ny][nx] = distance[y][x] + 1 #시작점으로부터 거리를 더한다.
+            #방문하지 않는 노드이면서 범위 안에 있을 때
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny] == '1' and distance[nx][ny] == 0:
+                q.append([nx, ny])
+                distance[nx][ny] = distance[x][y] + 1 #시작점으로부터 거리를 더한다.
 
     return distance[-1][-1]
 
