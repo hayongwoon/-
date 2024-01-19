d919b4e515c6091bb34afbc70717d034d8947cec
hayongwoon <40163691+hayongwoon@users.noreply.github.com>
2024-01-19 11:37:40 +0900

    [level 2] Title: 택배상자, Time: 373.35 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,11 +1,9 @@
 def solution(order):
     answer = 0
     tmp = []
-    i = 1
-    while i != len(order) + 1:
+    for i in range(1, len(order) + 1):
         tmp.append(i)
         while tmp and tmp[-1] == order[answer]:
             answer += 1
             tmp.pop()
-        i += 1
     return answer
